95396f66a59a225e455861530f2c1cea4aeb1da4
Tuan Nguyen <atuannguyen1101@gmail.com>
2018-08-30 11:35:08 -0400

    Initial commit

---
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.cs2340_68b.donationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
