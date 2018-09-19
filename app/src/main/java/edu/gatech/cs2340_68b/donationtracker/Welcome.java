91a5bac76766de06b0de30fa8a44d05bdccd10fc
Trinh Do <trinhiu21@gmail.com>
2018-09-18 22:51:50 -0400

    Design UI

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Welcome.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Welcome.java
@@ -1,25 +1,31 @@
 package edu.gatech.cs2340_68b.donationtracker;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class Welcome extends AppCompatActivity {
 
-    private Button login;
-    private Button register;
+
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+        ConstraintLayout current = (ConstraintLayout) findViewById(R.id.layout);
+        current.setBackgroundColor(Color.WHITE);
 
-        login = (Button)findViewById(R.id.welcomeLogin);
-        register = (Button)findViewById(R.id.welcomeRegister);
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
 
-        login.setOnClickListener(new View.OnClickListener() {
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Welcome.this, Login.class);
