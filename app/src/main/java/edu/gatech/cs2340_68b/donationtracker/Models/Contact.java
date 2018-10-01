df59c00ff5859b3c84127606e2bd41680b7e137d
Tuan Nguyen <atuannguyen1101@gmail.com>
2018-10-01 13:21:07 -0400

    Add Class: Contact, UserType

---
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Models/Contact.java
@@ -0,0 +1,87 @@
+package edu.gatech.cs2340_68b.donationtracker.Models;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emergencyContact;
+    private String addressLine;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String country;
+
+    public Contact() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmergencyContact() {
+        return emergencyContact;
+    }
+
+    public void setEmergencyContact(String emergencyContact) {
+        this.emergencyContact = emergencyContact;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
