a595cdd5e5412ca8076f23bfef5266b87d8425a9
Thinh Nguyen <tnntech@gatech.edu>
2018-10-01 18:17:53 -0400

    forget password logic

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Register/ForgetPassword.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Register/ForgetPassword.java
@@ -1,31 +1,60 @@
-package edu.gatech.cs2340_68b.donationtracker;
+package edu.gatech.cs2340_68b.donationtracker.Controllers.Register;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import edu.gatech.cs2340_68b.donationtracker.Controllers.Common.CustomDialog;
+import edu.gatech.cs2340_68b.donationtracker.Controllers.Common.VerifyFormat;
+
+
+import edu.gatech.cs2340_68b.donationtracker.Controllers.Login.Login;
+import edu.gatech.cs2340_68b.donationtracker.R;
 
 
 public class ForgetPassword extends AppCompatActivity {
-    private ActionBar actionBar;
-    private Button logout;
-
+    private Button resetButton;
+    private Button goBack;
+    private TextInputEditText userEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forget_password);
-        logout = (Button) findViewById(R.id.logoutButton);
-        logout.setOnClickListener(new View.OnClickListener() {
+        resetButton = (Button)findViewById(R.id.resetButton);
+        goBack = (Button)findViewById(R.id.goBack);
+        userEmail = findViewById(R.id.emailUser);
+
+        // Reset button click
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputEmail = userEmail.getText().toString().trim();
+                if (inputEmail.length() != 0 && VerifyFormat.verifyEmailFormat(inputEmail)) {
+                    // Move to reset page
+                    Intent intent = new Intent(ForgetPassword.this, resetPassword.class);
+//                    intent.putExtra(inputEmail, )
+                    startActivity(intent);
+                } else {
+                    AlertDialog.Builder alert = CustomDialog.errorDialog(ForgetPassword.this,
+                            "Format Error", "Email format is not correct");
+                    alert.create().show();
+                    return;
+                }
+            }
+        });
+
+        // Go back click
+        goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-        actionBar = getSupportActionBar();
-        actionBar.setTitle("Reset Your Password");
-//        actionBar.setIcon(R.drawable.icon);
-//        actionBar.setDisplayUseLogoEnabled(true);
+
     }
 }
