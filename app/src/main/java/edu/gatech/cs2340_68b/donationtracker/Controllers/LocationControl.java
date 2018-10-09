0bc90af3e30e5dab5a827e4864eaf488d177b611
Chan Lee <clee3243@gmail.com>
2018-10-08 20:33:57 -0400

    location page setup

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/LocationControl.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/LocationControl.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 import edu.gatech.cs2340_68b.donationtracker.Models.Location;
 import edu.gatech.cs2340_68b.donationtracker.Models.LocationList;
@@ -19,7 +20,7 @@ public class LocationControl {
     public static void readCSVFile() {
         try {
             //Open a stream on the raw file
-            InputStream is = new FileInputStream(new File("./raw/LocationData.csv"));
+            InputStream is = new FileInputStream(new File("./raw/locationdata.csv"));
             // InputStream is = new getResources().openRawResource(R.raw.LocationData);
 
             //From here we probably should call a model method and pass the InputStream
@@ -35,7 +36,7 @@ public class LocationControl {
             br1.close();
 
             // Array of locations with appropriate size
-            LocationList.locdata = new Location[length];
+            LocationList.locdata = new ArrayList<>();
 
             // Initialize each location element in the locdata array
             // with parsed tokens from the csv file
@@ -45,8 +46,8 @@ public class LocationControl {
             br2.readLine(); //get rid of header line
             while ((readline = br2.readLine()) != null) {
                 String[] token = line.split(",");
-                LocationList.locdata[idx] = new Location(token[0], token[1], token[2], token[3], token[4], token[5],
-                        token[6], token[7], token[8], token[9], token[10]);
+                LocationList.locdata.add(new Location(token[0], token[1], token[2], token[3], token[4], token[5],
+                        token[6], token[7], token[8], token[9], token[10]));
                 idx++;
             }
             br2.close();
---
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/LocationPage.java
@@ -0,0 +1,8 @@
+package edu.gatech.cs2340_68b.donationtracker.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class LocationPage extends AppCompatActivity {
+
+
+}
