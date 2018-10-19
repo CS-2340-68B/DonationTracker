7605116f75f01f899e28f71533b72c84b2df9660
Lan Le Tu <lan.letu96@gmail.com>
2018-10-19 00:37:39 -0400

    Donation detail id fix

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Location/DonationDetail.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Location/DonationDetail.java
@@ -12,6 +12,7 @@ public class DonationDetail extends AppCompatActivity {
     private ActionBar actionBar;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
