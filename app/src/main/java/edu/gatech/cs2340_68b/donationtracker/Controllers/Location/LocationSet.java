1c9be685cf07b31bb80b0c1ebbbf60342af737b9
Thinh Nguyen <tnntech@gatech.edu>
2018-10-18 23:05:18 -0400

    new add, update manifest

---
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Location/LocationSet.java
@@ -0,0 +1,4 @@
+package edu.gatech.cs2340_68b.donationtracker.Controllers.Location;
+
+public class LocationSet {
+}
---
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Models/Enum/StatesList.java
@@ -0,0 +1,4 @@
+package edu.gatech.cs2340_68b.donationtracker.Models.Enum;
+
+public class StatesList {
+}
