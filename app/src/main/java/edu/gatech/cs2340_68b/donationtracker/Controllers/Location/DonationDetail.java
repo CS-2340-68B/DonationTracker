11a057efd7afc046bdeb27d1d6e123316d0f74d0
Lan Le Tu <lan.letu96@gmail.com>
2018-10-19 00:50:08 -0400

    DonationDetail controller edit

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Location/DonationDetail.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Location/DonationDetail.java
@@ -5,12 +5,20 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import edu.gatech.cs2340_68b.donationtracker.R;
 
 public class DonationDetail extends AppCompatActivity {
     private ActionBar actionBar;
-
+    private EditText time;
+    private EditText location;
+    private EditText fullDescription;
+    private EditText shortDescription;
+    private EditText value;
+    private EditText comment;
+    private Spinner category;
 
 
     @Override
