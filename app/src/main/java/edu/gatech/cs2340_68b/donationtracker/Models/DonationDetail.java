10fbbdf03a5c795e4f930851cc1ca65d9a1db095
Lan Le Tu <lan.letu96@gmail.com>
2018-10-19 00:43:50 -0400

    Model DonationDetail

---
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Models/DonationDetail.java
@@ -0,0 +1,72 @@
+package edu.gatech.cs2340_68b.donationtracker.Models;
+
+public class DonationDetail {
+
+    private String time;
+    private  Location location;
+    private String fullDescription;
+    private String shortDescription;
+    private String value;
+    private Category category;
+    private String comment;
+
+    public DonationDetail {};
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getFullDescription() {
+        return fullDescription;
+    }
+
+    public void setFullDescription(String fullDescription) {
+        this.fullDescription = fullDescription;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+}
