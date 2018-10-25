940331957675e9fcb39d30ae916625e1f8093cab
Chan Lee <clee3243@gmail.com>
2018-10-24 23:27:59 -0400

    layout change

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/SearchMenu.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/SearchMenu.java
@@ -60,10 +60,6 @@ public class SearchMenu extends AppCompatActivity {
             }
         });
 
-
-
-
-
     }
 
 }
--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/SearchResult.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/SearchResult.java
@@ -5,5 +5,5 @@ import android.support.v7.app.AppCompatActivity;
 public class SearchResult extends AppCompatActivity {
 
     int searchTypeFlag = (int) getIntent().getSerializableExtra("SearchFlag");
-    
+
 }
