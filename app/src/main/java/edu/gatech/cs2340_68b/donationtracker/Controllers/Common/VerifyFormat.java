5a1f3f3c2b535147c03702bb96542543c164d685
Chan Lee <clee3243@gmail.com>
2018-11-06 20:07:47 -0500

    M10 Unit Test

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Common/VerifyFormat.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/Controllers/Common/VerifyFormat.java
@@ -9,11 +9,16 @@ public class VerifyFormat {
 
 
     public static boolean verifyPassword (String pass) {
+        if (pass == null || pass.length() == 0) {
+            return false;
+        }
+
         char character;
         boolean capitalFlag = false;
         boolean numberFlag = false;
         boolean letterFlag = false;
         boolean lengthFlag = false;
+        boolean specialFlag = false;
 
         if (pass.length() >= 8) {
             lengthFlag = true;
@@ -26,9 +31,13 @@ public class VerifyFormat {
             if (Character.isUpperCase(character) || Character.isLowerCase(character)) {
                 letterFlag = true;
             }
+            Character c = character;
+            if (c.toString().matches("[^a-z A-Z 0-9]")) {
+                specialFlag = true;
+            }
         }
 
-        return (capitalFlag && numberFlag && letterFlag && lengthFlag);
+        return (capitalFlag && numberFlag && letterFlag && lengthFlag && !specialFlag);
 
     }
 }
