b340f19dbbec0fec0522f9552a11233297c1114d
Lan Le Tu <lan.letu96@gmail.com>
2018-11-11 19:57:03 -0500

    Listener added to menu on the main page

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/Menu.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/Menu.java
@@ -1,22 +1,31 @@
 package edu.gatech.cs2340_68b.donationtracker.View;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.support.v4.app.Fragment;
 
 
 import java.util.Objects;
 
 import edu.gatech.cs2340_68b.donationtracker.R;
+import edu.gatech.cs2340_68b.donationtracker.View.locationViews.LocationListView;
+import edu.gatech.cs2340_68b.donationtracker.View.searchViews.SearchView;
 
 /**
  * Controller for the action bar menu
  */
 public class Menu extends AppCompatActivity {
     private ActionBarDrawerToggle aToggle;
+    private DrawerLayout drawer;
+
+
 
 
     @Override
@@ -24,6 +33,12 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.test_menu);
+
+        final Context contect = this;
+
+        drawer = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
         Toolbar aToolbar = findViewById(R.id.nav_actionbar);
         setSupportActionBar(aToolbar);
         DrawerLayout nDrawerLayout = findViewById(R.id.drawerLayout);
@@ -32,6 +47,43 @@ public class Menu extends AppCompatActivity {
         aToggle.syncState();
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        if (menuItem.getItemId() == R.id.nav_account) {
+                            Intent intent = new Intent(contect ,UserProfile.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_search) {
+                            Intent intent = new Intent(contect ,SearchView.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_location) {
+                            Intent intent = new Intent(contect ,LocationListView.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_history) {
+                            Intent intent = new Intent(contect ,UserProfile.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_logout) {
+                            finish();
+                        }
+
+
+
+                        // close drawer when item is tapped
+                        drawer.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
 
 
@@ -39,6 +91,13 @@ public class Menu extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        System.out.println(item.getItemId());
+        if (item.getItemId() == R.id.nav_account) {
+            Intent intent = new Intent(this,UserProfile.class);
+            this.startActivity(intent);
+            return true;
+
+        }
         if (aToggle.onOptionsItemSelected(item)) {
             return true;
         } else {
