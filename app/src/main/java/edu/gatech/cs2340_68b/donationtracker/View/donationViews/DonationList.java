4e5d3e03426479b81a55de98c2d20d8f0cb928ac
Lan Le Tu <lan.letu96@gmail.com>
2018-11-12 18:35:35 -0500

    Menu implemented in donation list and location detail

--- app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/donationViews/DonationList.java
+++ app/src/main/java/edu/gatech/cs2340_68b/donationtracker/View/donationViews/DonationList.java
@@ -1,11 +1,17 @@
 package edu.gatech.cs2340_68b.donationtracker.View.donationViews;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -34,6 +40,11 @@ import edu.gatech.cs2340_68b.donationtracker.Controllers.Common.DataListAdapter;
 import edu.gatech.cs2340_68b.donationtracker.Controllers.HttpUtils;
 import edu.gatech.cs2340_68b.donationtracker.Models.DonationDetail;
 import edu.gatech.cs2340_68b.donationtracker.R;
+import edu.gatech.cs2340_68b.donationtracker.View.Login;
+import edu.gatech.cs2340_68b.donationtracker.View.UserProfile;
+import edu.gatech.cs2340_68b.donationtracker.View.locationViews.LocationListView;
+import edu.gatech.cs2340_68b.donationtracker.View.searchViews.SearchHistory;
+import edu.gatech.cs2340_68b.donationtracker.View.searchViews.SearchView;
 
 import static edu.gatech.cs2340_68b.donationtracker.View.Welcome.currentUser;
 import static edu.gatech.cs2340_68b.donationtracker.View.Welcome.gson;
@@ -46,11 +57,65 @@ public class DonationList extends AppCompatActivity {
 
     private ListView donationListView;
     private String newLocation;
+    private ActionBarDrawerToggle aToggle;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donation_list);
+        final Context contect = this;
+
+        drawer = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        Toolbar aToolbar = findViewById(R.id.nav_actionbar);
+        setSupportActionBar(aToolbar);
+        DrawerLayout nDrawerLayout = findViewById(R.id.drawerLayout);
+        aToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
+        nDrawerLayout.addDrawerListener(aToggle);
+        aToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        if (menuItem.getItemId() == R.id.nav_account) {
+                            Intent intent = new Intent(contect ,UserProfile.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_search) {
+                            Intent intent = new Intent(contect ,SearchView.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_location) {
+                            Intent intent = new Intent(contect ,LocationListView.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_history) {
+                            Intent intent = new Intent(contect ,SearchHistory.class);
+                            startActivity(intent);
+                        }
+                        if (menuItem.getItemId() == R.id.nav_logout) {
+                            Intent intent = new Intent(contect, Login.class);
+                            startActivity(intent);
+                        }
+
+
+
+                        // close drawer when item is tapped
+                        drawer.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
         final String locationName = (String) getIntent().getSerializableExtra("PLACENAME");
         final String defaultLocation = (String) getIntent().getSerializableExtra("DEFAULT");
         donationListView = findViewById(R.id.donationList);
@@ -134,6 +199,22 @@ public class DonationList extends AppCompatActivity {
                 startActivity(detail);
             }
         });
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        System.out.println(item.getItemId());
+        if (item.getItemId() == R.id.nav_account) {
+            Intent intent = new Intent(this,UserProfile.class);
+            this.startActivity(intent);
+            return true;
+
+        }
+        if (aToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
 
     }
 }
